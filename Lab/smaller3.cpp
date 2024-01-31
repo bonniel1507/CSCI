d98534cbfaed7b500611b74dd28eeea8e6992e54
bonniel1507 <73480199+bonniel1507@users.noreply.github.com>
2024-01-31 04:31:22 +0000

    update

--- Lab/smaller3.cpp
+++ Lab/smaller3.cpp
@@ -2,7 +2,7 @@
 // bonnie.lei49@myhunter.cuny.edu
 // Course: CSCI 135
 // Instructor: Tong Yi
-// Assignment: Lab1A - Find the smaller of two integers.
+// Assignment: Lab1B - Find the smaller of three integers.
 
 #include <iostream>
 using namespace std;
@@ -16,12 +16,18 @@ int main() {
     int num2;
     cout << "Enter the second number: " << endl;
     cin >> num2;
+    // Ask for user input
+    int num3;
+    cout << "Enter the second number: " << endl;
+    cin >> num3;
     // Compare
-    cout << "The smaller of the two is ";
-    if (num1 > num2){
+    cout << "The smaller of the three is ";
+    if (num1 < num2 && num1 < num3){
+        cout << num1 << endl;
+    } else if (num2 < num1 && num2 < num3){
         cout << num2 << endl;
     } else {
-        cout << num1 << endl;
+        cout << num3 << endl;
     }
 
     return 0;
