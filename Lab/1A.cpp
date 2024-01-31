ee293b3b5ca734f4427ecf5ae9872dd8c0bd1f83
bonniel1507 <73480199+bonniel1507@users.noreply.github.com>
2024-01-30 22:58:59 -0500

    up

--- Lab/1A.cpp
+++ Lab/1A.cpp
@@ -3,3 +3,25 @@
 // Instructor: Tong Yi
 // Assignment: Lab1A - Find the smaller of two integers.
 
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int num1;
+	cout << "Enter the first number: " << endl;
+	cin >> num1;
+
+    int num2;
+	cout << "Enter the second number: " << endl;
+	cin >> num2;
+
+    cout << "The smaller of the two is ";
+    if (num1 > num2){
+        cout << num1;
+    } else {
+        cout << num2;
+    }
+
+	return 0;
+}
