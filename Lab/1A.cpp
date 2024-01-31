be8a65f451cb3ce67654558c251d15aa24593a73
bonniel1507 <73480199+bonniel1507@users.noreply.github.com>
2024-01-31 04:14:15 +0000

    up

--- Lab/1A.cpp
+++ Lab/1A.cpp
@@ -9,12 +9,12 @@ using namespace std;
 int main() {
 
     int num1;
-	cout << "Enter the first number: " << endl;
-	cin >> num1;
+    cout << "Enter the first number: " << endl;
+    cin >> num1;
 
     int num2;
-	cout << "Enter the second number: " << endl;
-	cin >> num2;
+    cout << "Enter the second number: " << endl;
+    cin >> num2;
 
     cout << "The smaller of the two is ";
     if (num1 > num2){
@@ -23,5 +23,5 @@ int main() {
         cout << num1 << endl;
     }
 
-	return 0;
+    return 0;
 }
