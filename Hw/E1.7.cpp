a6e296b73a02c22811b073516e059ed33882af31
bonniel1507 <73480199+bonniel1507@users.noreply.github.com>
2024-02-01 20:12:50 +0000

    add 1.7

---
+++ Hw/E1.7.cpp
@@ -0,0 +1,34 @@
+// Bonnie Lei
+// bonnie.lei49@myhunter.cuny.edu
+// Course: CSCI 135
+// Instructor: Tong Yi
+// Assignment: E1.7
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    // Ask for user input
+    string str1;
+    cout << "Enter a string: ";
+    cin >> str1;
+
+    // Ask for user input
+    string str2;
+    cout << "Enter a string: ";
+    cin >> str2;
+
+    // Ask for user input
+    string str3;
+    cout << "Enter a string: ";
+    cin >> str3;
+
+    // print
+    cout << str1 << endl;
+    cout << str2 << endl;
+    cout << str3 << endl;
+
+    return 0;
+}
