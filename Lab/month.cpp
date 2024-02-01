0a062b69963ebce5f6d5b917f3cd5ad330926bc5
bonniel1507 <73480199+bonniel1507@users.noreply.github.com>
2024-02-01 19:48:27 +0000

    update

--- Lab/month.cpp
+++ Lab/month.cpp
@@ -8,7 +8,37 @@
 using namespace std;
 
 int main() {
+    // Ask for user input
+    int year;
+    cout << "Enter year: ";
+    cin >> year;
 
+    // Ask for user input
+    int month;
+    cout << "Enter month: ";
+    cin >> month;
+    
+
+    if(month == 2){
+        if(year % 4 == 0 && (year % 400 == 0 || year % 100 != 0)){
+            cout << "29";
+        } else {
+            cout << "28";
+        }
+    } else if(month <= 7){
+        if(month % 2 == 1){
+            cout << "31";
+        } else {
+            cout << "30";
+        }
+    } else {
+        if(month % 2 == 0){
+            cout << "31";
+        } else {
+            cout << "30";
+        }
+    }
+    cout << " days" << endl;
 
     return 0;
 }
