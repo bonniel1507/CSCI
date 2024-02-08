5e20db1d57f199324d9346268e42d03ad586df32
bonniel1507 <73480199+bonniel1507@users.noreply.github.com>
2024-02-08 20:02:49 +0000

    1a

---
+++ Project/p1a.cpp
@@ -0,0 +1,50 @@
+// Bonnie Lei
+// bonnie.lei49@myhunter.cuny.edu
+// Course: CSCI 135
+// Instructor: Tong Yi
+// Assignment: Project 1a
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+
+    // declare variables
+    char operators[] = {'+', '-', '*', '/', '%'};
+    int num1 = rand()%10;
+    char sign = operators[rand()%5];
+    int num2;
+    int ans;
+
+    // checking the sign for next step
+    if(sign == '+'){
+        num2 = rand()%9;
+        ans = num1 + num2;
+    } else if(sign == '-'){
+        num2 = rand()%9;
+        ans = num1 - num2;
+    } else if(sign == '*'){
+        num2 = rand()%9;
+        ans = num1 * num2;
+    } else if(sign == '/'){
+        num2 = rand()%8+1;
+        ans = num1 / num2;
+    } else if(sign == '%'){
+        num2 = rand()%8+1;
+        ans = num1 % num2;
+    }
+    
+    int input;
+    cout << "What is " << num1 << " " << sign << " " << num2 << "? ";
+    cin >> input;
+
+    // compare user input and answer
+    if(input == ans){
+        cout << "True" << endl;
+    } else {
+        cout << "False" << endl;
+    }
+
+    return 0;
+}
