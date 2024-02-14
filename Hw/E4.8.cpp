7231a6fcdc94308468c7d5f12119fd242ef59fc5
bonniel1507 <73480199+bonniel1507@users.noreply.github.com>
2024-02-14 03:25:44 +0000

    e4.8

---
+++ Hw/E4.8.cpp
@@ -0,0 +1,21 @@
+// Bonnie Lei
+// bonnie.lei49@myhunter.cuny.edu
+// Course: CSCI 135
+// Instructor: Tong Yi
+// Assignment: E4.8
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string word;
+    cout << "Please enter a word: ";
+    cin >> word;
+
+    for(int i = 0; i < word[i]; i++){
+        cout << word[i] << endl;
+    }
+
+    return 0;
+}
