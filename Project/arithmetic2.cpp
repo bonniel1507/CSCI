2f3697b5eaaa517cbe036913de9c005c7bf74a81
bonniel1507 <73480199+bonniel1507@users.noreply.github.com>
2024-02-15 18:33:15 +0000

    p1

---
+++ Project/arithmetic2.cpp
@@ -0,0 +1,83 @@
+// Bonnie Lei
+// bonnie.lei49@myhunter.cuny.edu
+// Course: CSCI 135
+// Instructor: Tong Yi
+// Assignment: Project 1
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    srand(time(NULL));
+    // declare variables
+    char operators[] = {'+', '-', '*', '/', '%'};
+    int s = sizeof(operators); // / sizeof(operators[0]); // 5/1=5
+    int idx = rand() % s;
+    int num1, num2, ans, questions;
+    char sign;
+
+    cout << "Enter the total number of questions to ask: ";
+    cin >> questions;
+
+    double percent, corrects = 0.0;
+    
+    while(questions <= 0){
+        cout << "The number of questions cannot be zero or negative. Re-enter: ";
+        cin >> questions;
+    }
+
+    for(int i = 1; i <= questions; i++){
+        num1 = rand()%10;
+        sign = operators[idx];
+        if(sign == '+'){
+            num2 = rand()%10;
+            ans = num1 + num2;
+        } else if(sign == '-'){
+            num2 = rand()%10;
+            ans = num1 - num2;
+        } else if(sign == '*'){
+            num2 = rand()%10;
+            ans = num1 * num2;
+        } else if(sign == '/'){
+            num2 = 1 + rand()%9;
+            ans = num1 / num2;
+        } else {
+            num2 = 1 + rand()%9;
+            ans = num1 % num2;
+        }
+
+        string inputs;
+        cout << "\n(" << i << ") " "what is " << num1 << " " << sign << " " << num2 << "? ";
+        cin >> inputs;
+        int input = stoi(inputs);
+        
+        // compare user input and answer
+        if(input == ans){
+            cout << "True" << endl;
+            corrects++;
+        } else {
+            cout << "False" << endl;
+        }
+    }
+
+    // cout << corrects << " " << questions << endl;
+    percent = (corrects / questions)*100;
+    cout << "\npercentage correct: " << percent << "%" << endl;
+
+    cout << "letter grade: ";
+    if(percent >= 90.0){
+        cout << "A" << endl;
+    } else if(percent >= 80.0){
+        cout << "B" << endl;
+    } else if(percent >= 70.0){
+        cout << "c" << endl;
+    } else if(percent >= 60.0){
+        cout << "D" << endl;
+    } else {
+        cout << "F" << endl;
+    }
+
+    return 0;
+}
