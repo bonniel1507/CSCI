7bd34f7b17f0e4a192341cb23c6a0ad262c7ea46
bonniel1507 <73480199+bonniel1507@users.noreply.github.com>
2024-02-15 14:53:17 -0500

    Update minmax.cpp

--- Lab/minmax.cpp
+++ Lab/minmax.cpp
@@ -11,36 +11,36 @@
 using namespace std;
 
 int main() {
-    ifstream fin("Current_Reservoir_Levels.tsv");
+	ifstream fin("Current_Reservoir_Levels.tsv");
+	
+	if(fin.fail()) {
+		cerr << "File cannot be opened for reading." << endl;
+		exit(1); // exit if failed to open the file
+	}
 
-    if(fin.fail()) {
-        cerr << "File cannot be opened for reading." << endl;
-        exit(1); // exit if failed to open the file
-    }
-
-    string junk;        // new string variable
-    getline(fin, junk); // read one line from the file
-
-    // declare variables
-    string date;
-    double eastSt, min = INT_MAX, max = INT_MIN;
-    
-    while(fin >> date >> eastSt) { 
+	string junk;        // new string variable
+	getline(fin, junk); // read one line from the file
+	
+	// declare variables
+	string date;
+	double eastSt, min = INT_MAX, max = INT_MIN;
+	
+	while(fin >> date >> eastSt) { 
 		fin.ignore(INT_MAX, '\n'); //skips to the end of line, 
-                            //ignorring the remaining columns 
-        // compare
-        if(eastSt < min) {
+		//ignorring the remaining columns 
+		// compare
+		if(eastSt < min) {
 			min = eastSt;
-        } 
-        if(eastSt > max) {
+		} 
+		if(eastSt > max) {
 			max = eastSt;
-        }
-    }  
-    // print
-    cout << "minimum storage in East basin: " << min << " billion gallons" << endl;
-    cout << "MAXimum storage in East basin: " << max << " billion gallons" << endl;
-
-    fin.close();
-
-    return 0;
+		}
+	}  
+	// print
+	cout << "minimum storage in East basin: " << min << " billion gallons" << endl;
+	cout << "MAXimum storage in East basin: " << max << " billion gallons" << endl;
+	
+	fin.close();
+	
+	return 0;
 }
