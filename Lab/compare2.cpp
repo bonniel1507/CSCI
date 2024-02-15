d3c90c884b6dae2090b1cf36f33b91fa3f7ab761
bonniel1507 <73480199+bonniel1507@users.noreply.github.com>
2024-02-15 21:19:25 +0000

    up

--- Lab/compare2.cpp
+++ Lab/compare2.cpp
@@ -16,9 +16,11 @@ int main() {
         cerr << "File cannot be opened for reading." << endl;
         exit(1); // exit if failed to open the file
     }
+
     string junk;        // new string variable
     getline(fin, junk); // read one line from the file
 
+    int dates = 0;
     string date, start, end;
     double eastEl, westEl;
 
@@ -28,9 +30,20 @@ int main() {
     cin >> end;
 
     while(fin >> date >> eastEl >> westEl) { 
+        
         fin.ignore(INT_MAX, '\n'); //skips to the end of line, 
                             //ignorring the remaining columns 
 
+        // for example, to print the date and East basin storage:
+        // cout << date << " " << eastSt << endl;
+
+        // if(date == start) {
+        //     dates++;
+        // } else if(date > end) {
+        //     break;
+        // }
+
+        // for(int i = 0; i < dates; i++) {
         while(date >= start && date <= end){
             cout << date;
             if(eastEl > westEl) {
