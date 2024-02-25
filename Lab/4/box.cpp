b332bb465568c5f82c81c46acc41085f65b69028
bonniel1507 <73480199+bonniel1507@users.noreply.github.com>
2024-02-25 00:14:48 +0000

    task a

---
+++ Lab/4/box.cpp
@@ -0,0 +1,28 @@
+// Bonnie Lei
+// bonnie.lei49@myhunter.cuny.edu
+// Course: CSCI 135
+// Instructor: Tong Yi
+// Assignment: task a
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // declare variables
+    int w, h;
+    // user input
+    cout << "Input width: ";
+    cin >> w;
+    cout << "Input height: ";
+    cin >> h;
+    // print
+    for(int i = 0; i < h; i++){
+        for(int j = 0; j < w; j++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
+ 
