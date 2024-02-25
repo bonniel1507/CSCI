780c2ddd6abe4649fad43b139e2c00dae4581d86
bonniel1507 <73480199+bonniel1507@users.noreply.github.com>
2024-02-25 03:08:45 +0000

    task f

---
+++ Lab/4/trapezoid.cpp
@@ -0,0 +1,36 @@
+// Bonnie Lei
+// bonnie.lei49@myhunter.cuny.edu
+// Course: CSCI 135
+// Instructor: Tong Yi
+// Assignment: task f
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // declare variables
+    int w, h;
+    // user input
+    cout << "Input width: ";
+    cin >> w;
+    cout << "Input height: ";
+    cin >> h;
+    // print
+    if(h > w/2){
+        cout << "Impossible shape!" << endl;
+    } else {
+        cout << "\nShape:";
+        for(int i = 0; i < h; i++){
+            cout << "\n";
+            for(int j = 0; j < w; j++){
+                if(j >= i && j < w-i){
+                    cout << "*";
+                } else {
+                    cout << " ";
+                }
+            }
+        }
+    }
+
+    return 0;
+}
