9dd512742da9047eaf652f74068c0ed6b1aa66b4
Bonnie Lei <bonnielei91@gmail.com>
2024-02-29 12:56:54 -0500

    update repo

--- Hw/E5.6.cpp
+++ Hw/E5.6.cpp
@@ -10,8 +10,10 @@ using namespace std;
 
 string middle(string str){
 
+    // if str.length is odd
     string mid = str.substr(str.length()/2,1);
 
+    // if str.length is even
     if(str.length() % 2 == 0){
         mid = str.substr(str.length()/2-1,2);
     }
@@ -23,6 +25,7 @@ int main() {
     cout << "Enter a string: ";
     cin >> str;
 
+    // pass through the method
     string mid = middle(str);
     cout << mid << endl;
 
