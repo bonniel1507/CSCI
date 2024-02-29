bb9342d1056177ba834754ce1fa9db072b4c0253
Bonnie Lei <bonnielei91@gmail.com>
2024-02-29 12:55:33 -0500

    update repo

--- Hw/E5.6.cpp
+++ Hw/E5.6.cpp
@@ -5,16 +5,26 @@
 // Assignment: e5.6
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 string middle(string str){
-    if(str.length()){
-        
+
+    string mid = str.substr(str.length()/2,1);
+
+    if(str.length() % 2 == 0){
+        mid = str.substr(str.length()/2-1,2);
     }
+    return mid;
 }
 
 int main() {
+    string str;
+    cout << "Enter a string: ";
+    cin >> str;
 
+    string mid = middle(str);
+    cout << mid << endl;
 
     return 0;
 }
