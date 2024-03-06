ba645eb5f4808425893c0772bbf2ebba44951700
bonniel1507 <73480199+bonniel1507@users.noreply.github.com>
2024-03-06 05:08:58 +0000

    Wed, Mar 6, 2024, 12:08 AM -05:00

---
+++ Hw/E5.14.cpp
@@ -0,0 +1,23 @@
+// Bonnie Lei
+// bonnie.lei49@myhunter.cuny.edu
+// Course: CSCI 135
+// Instructor: Tong Yi
+// Assignment: e5.14
+
+#include <iostream>
+using namespace std;
+
+void sort2(int a, int b){
+	if(a > b){
+		swap(a, b);
+	}
+}
+
+int main() {
+
+	int a = 9, b = 3;
+	sort2(a, b);
+	cout << a;
+
+    return 0;
+}
