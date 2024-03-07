0a05cea18feb6d98ca866716df01d6568d1940a6
bonniel1507 <73480199+bonniel1507@users.noreply.github.com>
2024-03-07 14:10:40 -0500

    Update unindent.cpp

--- Lab/7/unindent.cpp
+++ Lab/7/unindent.cpp
@@ -1,1 +1,21 @@
+// Bonnie Lei
+// bonnie.lei49@myhunter.cuny.edu
+// Course: CSCI 135
+// Instructor: Tong Yi
+// Assignment: lab 7a
 
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+string removeLeadingSpaces(string line){
+
+}
+
+int main() {
+
+	string line;
+	removeLeadingSpaces(line);
+
+    return 0;
+}
