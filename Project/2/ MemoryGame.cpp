712d6b588e0b9077ed68335584f28510c60e877a
Bonnie Lei <bonnielei91@gmail.com>
2024-03-13 23:17:20 -0400

    update repo

---
+++ Project/2/ MemoryGame.cpp
@@ -0,0 +1,34 @@
+// Bonnie Lei
+// bonnie.lei49@myhunter.cuny.edu
+// Course: CSCI 135
+// Instructor: Tong Yi
+// Assignment:
+
+#include <iostream>
+using namespace std;
+
+#include "MemoryGame.hpp"
+
+MemoryGame::MemoryGame() {
+//TODO: place your code here
+}
+
+MemoryGame::MemoryGame(int numPairs, int numSlots) {
+//TODO: place your code here
+}
+
+MemoryGame::MemoryGame(string* words, int size, int numSlots) {
+//TODO: place your code here
+}
+
+MemoryGame::~MemoryGame() {
+//TODO: place your code here
+}
+
+void MemoryGame::randomize() {
+//TODO: placeholder, do not need to implement in Task A
+}
+
+void MemoryGame::display() const {
+//TODO: placeholder, do not need to implement in Task A
+}
