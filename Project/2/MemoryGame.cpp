a21c5a6354f55eb31be5e8f4254f327d2e68a4d5
Bonnie Lei <bonnielei91@gmail.com>
2024-03-21 00:21:00 -0400

    update repo

--- Project/2/MemoryGame.cpp
+++ Project/2/MemoryGame.cpp
@@ -25,16 +25,16 @@ MemoryGame::MemoryGame(int numPair, int numSlot) {
 		numSlots = 8;
 	}
 
-	int *arr = new int[numSlots];
+	string *arr = new string[numSlots];
 	bool *arr2 = new bool[numSlots];
 
-	// srand(time(NULL));
-	for(int i = 0; arr.size(); i+=2){
+	srand(time(NULL));
+	for(int i = 0; numSlots; i+=2){
 		if(i >= numPairs*2){
 			break;
 		}
 
-		int r = rand() % 1000;
+		string r = to_string(rand() % 1000);
 		arr[i] = r;
 		arr[i+1] = r;
 		arr2[i] = false;
@@ -47,28 +47,28 @@ MemoryGame::MemoryGame(int numPair, int numSlot) {
 
 MemoryGame::MemoryGame(string* words, int size, int numSlot) {
 //TODO: place your code here
-	if(numSLot <= 0 || numSlot < size*2){
+	if(numSlot <= 0 || numSlot < size*2){
 		numSlots = size*2;
 	} else {
-		numSLots = numSlot;
+		numSlots = numSlot;
 		numPairs = size;
 	}
 
-	string *arr = new int[numSlots];
+	string *arr = new string[numSlots];
 	bool *arr2 = new bool[numSlots];
 
-	int j = 0;
+	// int j = 0;
 
-	for(int i = 0; arr.size(); i+=2){
+	for(int i = 0; numSlots; i+=2){
 		if(i >= numPairs*2){
 			break;
 		}
 
-		arr[i] = words[j];
-		arr[i+1] = words[j];
+		arr[i] = words[i/2];
+		arr[i+1] = words[i/2];
 		arr2[i] = false;
 		arr2[i+1] = false;
-		j++;
+		// j++;
 	}
 
 	this->values = arr;
