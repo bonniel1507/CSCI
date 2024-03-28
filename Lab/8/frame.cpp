1f9310108a6795ad47c1734745cdfc640d0e628a
Bonnie Lei <bonnielei91@gmail.com>
2024-03-28 15:04:00 -0400

    update repo

--- Lab/8/frame.cpp
+++ Lab/8/frame.cpp
@@ -95,10 +95,14 @@ int main() {
 
 	for(int row = 0; row < h; row++) {
 		for(int col = 0; col < w; col++) {
-			if((row == h/2 + h/4 || row == h/4 && col < w/2 + w/4 && col > w/4) || (col == w/2 + w/4 || col == w/4 && row < h/2 + h/4 && row > h/4)){
-				out[row][col] = 255;
+			if(row < h/2 + h/4 && row > h/4 && col < w/2 + w/4 && col > w/4){
+				if(row == h/2 + h/4 || row == h/4 || col == w/2 + w/4 || col == w/4){
+					out[row][col] = 255;
+				} else {
+					out[row][col] = img[row][col];
+				}
 			} else {
-				out[row][col] = img[row][col];
+					out[row][col] = img[row][col];
 			}
 		}
 	}
