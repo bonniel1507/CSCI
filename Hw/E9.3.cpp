4fe45670406122baa514132c8a4511264f1cfed2
Bonnie Lei <bonnielei91@gmail.com>
2024-04-15 13:47:54 -0400

    update repo

--- Hw/E9.3.cpp
+++ Hw/E9.3.cpp
@@ -7,6 +7,34 @@
 #include <iostream>
 using namespace std;
 
+int get_first_switch_state(){
+    return
+}
+
+int get_second_switch_state(){
+    return
+}
+
+int get_lamp_state(){
+    return
+}
+
+void toggle_first_switch(){
+    if(){
+
+    } else {
+
+    }
+}
+
+void toggle_second_switch(){
+    if(){
+
+    } else {
+
+    }
+}
+
 int main() {
 
 
