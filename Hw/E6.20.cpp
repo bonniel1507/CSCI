4c64e8fd26a381f13db57a5ced059054505e8297
Bonnie Lei <bonnielei91@gmail.com>
2024-04-18 16:44:19 -0400

    update repo

--- Hw/E6.20.cpp
+++ Hw/E6.20.cpp
@@ -19,10 +19,43 @@ then merge_sorted returns the vector
 */
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int main() {
+vector<int> append(vector<int> a, vector<int> b){
+    vector<int> c;
+    for(int i = 0; i < a.size(); i++){
+        c.push_back(a[i]);
+    }
+    for(int i = 0; i < b.size(); i++){
+        c.push_back(b[i]);
+    }
+    return c;
+}
 
+vector<int> merge_sorted(vector<int> a, vector<int> b){
+    vector<int> c = append(a, b);
+    for(int i = 1; i < c.size(); i++){
+        for(int j = 0; j < c.size()-i; j++){
+            int temp = c[j];
+            if(c[j] > c[j+1]){
+                c[j] = c[j+1];
+                c[j+1] = temp;
+            }
+        }
+    }
+    return c;
+}
+
+int main() {
+    vector<int> a = {1, 4, 9, 16};
+    vector<int> b = {9, 7, 4, 9, 11};
+
+    vector<int> c = merge_sorted(a, b);
+
+    for(int i = 0; i < c.size(); i++){
+        cout << c[i] << " ";
+    }
 
     return 0;
 }
