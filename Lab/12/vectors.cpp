502d74c8a8ba8ae0b9a47e132e98d8ecfdd4b224
Bonnie Lei <bonnielei91@gmail.com>
2024-04-27 17:32:01 -0400

    update repo

---
+++ Lab/12/vectors.cpp
@@ -0,0 +1,21 @@
+// Bonnie Lei
+// bonnie.lei49@myhunter.cuny.edu
+// Course: CSCI 135
+// Instructor: Tong Yi
+// Assignment: lab 12a
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> makeVector(int n){
+    vector<int> v(n);
+    return v;
+
+}
+
+int main() {
+
+
+    return 0;
+}
