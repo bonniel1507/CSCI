789694639012c3b71c087c93191359668d41c214
Bonnie Lei <bonnielei91@gmail.com>
2024-04-30 19:28:16 -0400

    update repo

--- Lab/12/fusion.cpp
+++ Lab/12/fusion.cpp
@@ -2,7 +2,7 @@
 // bonnie.lei49@myhunter.cuny.edu
 // Course: CSCI 135
 // Instructor: Tong Yi
-// Assignment: lab 12b
+// Assignment: lab 12c
 
 #include <iostream>
 #include <vector>
@@ -26,8 +26,25 @@ vector<int> goodVibes(const vector<int> v){
     return e;
 }
 
+void gogeta(vector<int> &goku, vector<int> &vegeta){
+    while(vegeta.size() > 0){
+        goku.push_back(vegeta[0]);
+        vegeta.erase(vegeta.begin());
+    }
+}
+
 int main() {
 // comment
 // comment
+
+    vector<int> v1{1,2,3};
+    vector<int> v2{4,5};
+
+    gogeta(v1, v2); // v1 is now [1,2,3,4,5] and v2 is empty.
+
+    for(int i = 0; i < v1.size(); i++){
+        cout << v1[i] << " ";
+    }
+
     return 0;
 }
