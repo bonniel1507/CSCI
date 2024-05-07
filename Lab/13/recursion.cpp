af83c84a016c9ff7a97df652949d237041a623db
Bonnie Lei <bonnielei91@gmail.com>
2024-05-07 12:28:54 -0400

    update repo

--- Lab/13/recursion.cpp
+++ Lab/13/recursion.cpp
@@ -40,27 +40,27 @@ bool isAlphanumeric(string s){
     }
 }
 
+bool nestedParens(string s){
+    if(s.length() == 0){
+        return true;
+    } else {
+        if(s[0] == '(' && s[s.length()-1] == ')' ){
+            return nestedParens(s.substr(1, s.length()-2));
+        } else {
+            return false;
+        }
+    }
+}
+
 int main() {
-    int size = 10;
-    int *arr = new int[size]; // allocate array dynamically
-    arr[0] = 12;
-    arr[1] = 17;
-    arr[2] = -5;
-    arr[3] = 3;
-    arr[4] = 7;
-    arr[5] = -15;
-    arr[6] = 27;
-    arr[7] = 5;
-    arr[8] = 13;
-    arr[9] = -21;
+    cout << nestedParens("((()))") << endl;      // true (1)
+    cout << nestedParens("()") << endl;          // true (1)
+    cout << nestedParens("") << endl;            // true (1)
 
-    int sum1 = sumArray(arr, size); // Add all elements
-    cout << "Sum is " << sum1 << endl;  // Sum is 43
-
-    int sum2 = sumArray(arr, 5); // Add up first five elements
-    cout << "Sum is " << sum2 << endl;  // Sum is 34
-
-    delete[] arr;
+    cout << nestedParens("(((") << endl;         // false (0)
+    cout << nestedParens("(()") << endl;         // false (0)
+    cout << nestedParens(")(") << endl;          // false (0)
+    cout << nestedParens("a(b)c") << endl;       // false (0)
 
     return 0;
 }
